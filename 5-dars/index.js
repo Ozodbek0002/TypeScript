609d4e09e1f4b325f774de1b1e05f82960161cfe
Ozodbek Ozodov <o.ozodov@uzinfocom.uz>
2025-02-01 13:20:40 +0500

    first commit

---
+++ 5-dars/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let numbers = [1, 2, 3, 4];
+let strings = ["hello", "world"];
+let mix = [1, "hello"];
+//# sourceMappingURL=index.js.map
---
+++ 5-dars/index.ts
@@ -0,0 +1,3 @@
+let numbers: number[] = [1, 2, 3, 4];
+let strings: string[] = ["hello", "world"];
+let mix: (number | string)[]= [1, "hello"];
