bee7217998b85bef74ac867402eafd674ab9db7c
Ozodbek Ozodov <o.ozodov@uzinfocom.uz>
2025-03-06 15:14:26 +0500

    14-dars

--- 14-dars/index.ts
+++ 14-dars/index.ts
@@ -3,3 +3,17 @@
 
 // extends
 
+// type A<T> = T;
+// type B = A<string>;
+// type C = A<number>;
+// type D = A<"js">;
+
+
+type A<T extends string> = T;
+type B = A<string>;
+// type C = A<number>; // error
+type D = A<"js">; // js
+
+
+
+
